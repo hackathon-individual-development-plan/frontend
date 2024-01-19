1a90ee5d84539b56635c347ad1449a87028c098c
Kseniia Tikhonova <tikhonovakb@gmail.com>
2024-01-19 15:39:48 +0100

    create Employees and Create plan pages

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
-import BriefInfoCard from '../BriefInfoCard/BriefInfoCard.jsx';
+// import BriefInfoCard from '../BriefInfoCard/BriefInfoCard.jsx';
+// import CreatePlan from '../../pages/CreatePlan/CreatePlan.jsx';
+import Employees from '../../pages/Employees/Employees.jsx';
 
 function App() {
   return (
     <div className="page">
-      <BriefInfoCard />
+      {/* <BriefInfoCard /> */}
+      {/* <CreatePlan /> */}
+      <Employees />
     </div>
   );
 }
