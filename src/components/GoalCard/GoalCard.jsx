1914c9f7f5564fdd138ffcf7b7562ef3a857b494
SofiaFrikina <sofi.frikina@yandex.ru>
2024-01-23 00:33:41 +0300

    tasks comments and list pf goals

--- src/components/GoalCard/GoalCard.jsx
+++ src/components/GoalCard/GoalCard.jsx
@@ -1,3 +1,4 @@
+/*
 import React from 'react';
 import './GoalCard.css';
 
@@ -26,32 +27,23 @@ function GoalCard(card) {
         </div>
         <ol className="card__list-items">
           {card.tasks
-          && card.tasks.map((item, index) => (
+            && card.tasks.map((item, index) => (
               <li className="card__list-item" key={index}>
                 <p className="card__list-item-name">{item.name}</p>
               </li>
-          ))}
+            ))}
         </ol>
       </section>
       <section className="card__list card__list-padding-none">
         <div className="card__list-header">
           <button className="card__list-button"></button>
           <p className="card__field-name">
-            Комментарии <span>1</span>
+            Комментарии <span>{card.comments.length}</span>
           </p>
         </div>
-        {/* <ul className="card__message-list">
-          <li className="card__message-item">
-            <img className="card__message-photo" src="/images/photo-xs.svg" />
-            <div className="card__message-info">
-              <p className="card__message-name">Артемьев Олег Иванович</p>
-              <p className="card__message-text">Начал проходить тренинг</p>
-            </div>
-          </li>
-        </ul> */}
         <ul className="card__message-list">
           {card.comments
-          && card.comments.map((item, index) => (
+            && card.comments.map((item, index) => (
               <li className="card__message-item" key={index}>
                 <img className="card__message-photo" src={item.avatar} />
                 <div className="card__message-info">
@@ -59,15 +51,16 @@ function GoalCard(card) {
                   <p className="card__message-text">{item.message}</p>
                 </div>
               </li>
-          ))}
+            ))}
         </ul>
 
         <section className="card__textarea">
           <textarea className="card__textarea-field" placeholder="Добавьте комментарий"></textarea>
-          {/* <button className="card__textarea-button"></button> */}
+          /* <button className="card__textarea-button"></button> */
+/*
         </section>
       </section>
     </div>
   );
 }
-export default GoalCard;
+export default GoalCard; */
--- src/components/GoalCardEdit/GoalCardEdit.jsx
+++ src/components/GoalCardEdit/GoalCardEdit.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './GoalCardEdit.css';
 import { useForm } from 'react-hook-form';
 import DateInput from '../DateInput/DateInput.jsx';
@@ -7,6 +7,7 @@ import DropdownButton from '../DropdownButton/DropdownButton.jsx';
 function GoalCardEdit({ cardTarget }) {
   const { register, handleSubmit } = useForm();
   const [isActiveTasks, setActiveTasks] = useState(false);
+  const [isActiveMessages, setActiveMessages] = useState(false);
   const onSubmit = (data) => {
     console.log(data);
     alert('Сохранено');
@@ -54,24 +55,24 @@ function GoalCardEdit({ cardTarget }) {
         </section>
         <section className="card__list">
           <div className="card__list-header">
-            <button type='button' onClick={() => setActiveTasks(!isActiveTasks)} className={isActiveTasks ? 'card__list-button card__list-button_active' : 'card__list-button'}></button>
+            <button type='button' onClick={() => setActiveMessages(!isActiveMessages)} className={isActiveMessages ? 'card__list-button card__list-button_active' : 'card__list-button'}></button>
             <p className="card__list-title">
               Задачи <span>{cardTarget.tasks.length}</span>
             </p>
           </div>
-          <div className={isActiveTasks ? 'card__list-tasks card__list-tasks_active' : 'card__list-tasks'}>
-            <ol className='card__list-items'>
-              {(cardTarget.tasks).map((item) => (
+          <div className={isActiveMessages ? 'card__list-tasks card__list-tasks_active' : 'card__list-tasks'}>
+            <ol className="card__list-items">
+              {cardTarget.tasks.map((item) => (
                 <li key={item.id} className="card__list-item">
                   <p className="card__list-item-name">{item.task}</p>
                 </li>
-              ))
-              }
+              ))}
             </ol>
             <div className="card__list-add">
-              <button type='button' className="card__list-add-button"></button>
+              <button type="button" className="card__list-add-button"></button>
               <input
-                {...(register('task'), {
+                {...(register('task'),
+                {
                   required: true,
                 })}
                 className="card__input card__input_white card__input_border-none"
@@ -85,22 +86,24 @@ function GoalCardEdit({ cardTarget }) {
 
         <section className="card__list card__list-padding-none">
           <div className="card__list-header">
-            <button type="button" className="card__list-button"></button>
+            <button type='button' onClick={() => setActiveTasks(!isActiveTasks)} className={isActiveTasks ? 'card__list-button card__list-button_active' : 'card__list-button'}></button>
             <p className="card__list-title">
               Комментарии <span>{cardTarget.comments.length}</span>
             </p>
           </div>
-          <ul className="card__message-list">
-            {cardTarget.comments.map((item) => (
-              <li key={item.id} className="card__message-item">
-                <img className="card__message-photo" src={item.avatar} />
-                <div className="card__message-info">
-                  <p className="card__message-name">{item.name}</p>
-                  <p className="card__message-text">{item.comment}</p>
-                </div>
-              </li>
-            ))}
-          </ul>
+          <div className={isActiveTasks ? 'card__list-messages card__list-messages_active' : 'card__list-messages'}>
+            <ul className="card__message-list">
+              {cardTarget.comments.map((item) => (
+                <li key={item.id} className="card__message-item">
+                  <img className="card__message-photo" src={item.avatar} />
+                  <div className="card__message-info">
+                    <p className="card__message-name">{item.name}</p>
+                    <p className="card__message-text">{item.comment}</p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
 
           <section className="card__textarea">
             <textarea className="card__textarea-field" placeholder="Добавьте комментарий"></textarea>
