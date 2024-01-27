c5120654709057d04cb302b7ad5e30ae138394c1
SvetAlexa <dazzle@bk.ru>
2024-01-27 03:09:02 +0300

    refactor employees for server BD

--- src/components/SearchInput/SearchInput.jsx
+++ src/components/SearchInput/SearchInput.jsx
@@ -30,12 +30,12 @@ export default function SearchInput() {
         <div className='search__dropdown'>
           {filteredEmployeesList.filter((item) => {
             const searchTermInput = searchQuery.toLowerCase();
-            const name = item.name.toLowerCase();
+            const name = item.employee.fio.toLowerCase();
             return searchTermInput && name.startsWith(searchTermInput) && name !== searchTermInput;
           })
             .slice(0, 10)
-            .map((item) => (<div className='search__dropdown-row' onClick={() => onSearch(item.name)} key={item.id}>
-              {item.name}
+            .map((item) => (<div className='search__dropdown-row' onClick={() => onSearch(item.employee.fio)} key={item.employee.id}>
+              {item.employee.fio}
             </div>))}
         </div>
       </div>
